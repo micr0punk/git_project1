f68a9d686980f0df30f6c663d0e60e33bb44f5f5
micr0punk <micromacropunk@gmail.com>
2024-12-23 00:12:33 +0300

    added a func

--- program.py
+++ program.py
@@ -1,9 +1,14 @@
+def my_superfunction():
+    print('What an awesome print!')
+
+
 def main():
     print('My first git program')
     print('My first git program')
     print('Another change')
     print('MORE PRINTS')
     print('UFO came and added this line')
+    my_superfunction()
 
 if __name__ == '__main__':
     main()
